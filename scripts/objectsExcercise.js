2e86c2134778a48ada94c4f091cfe1c9f72e892d
ejdunia <ejdunia@gmail.com>
2022-05-03 23:30:38 +0100

    function for adding books working

--- scripts/objectsExcercise.js
+++ scripts/objectsExcercise.js
@@ -1,29 +1,53 @@
-function Book(title, author, pages, read) {
-    this.title = title;
+function Book(author, title, pages) {
     this.author = author;
+    this.title = title;
     this.pages = pages;
-    this.read = read;
+    // this.read = read;
     this.info = function () {
-        return `The ${this.title} by ${this.author},  ${this.pages}, ${this.read}`;
+        return `The ${this.title} by ${this.author},  ${this.pages}, ${this.pages}`;
     };
 }
+const hobbit = new Book("J.R.R. Tolkien", "Hobbit", "295 pages", "not read");
 
-const hobbit = new Book("Hobbit", "J.R.R. Tolkien", "295 pages", "not read");
-let myLibrary = [hobbit];
+let myLibrary = [hobbit, hobbit];
+myLibrary.forEach(createBookCard);
 
 const bookForm = document.querySelector("#bookForm");
-const addBtn = document.querySelector(".addBtn");
+const addBookBtn = document.querySelector(".addBookBtn");
 const formContainer = document.querySelector(".formContainer");
 const addBook = document.querySelector(".addBook");
 
-addBtn.addEventListener("click", () =>
+bookForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // create a new formdata object
+
+    const formData = new FormData(bookForm);
+    const author = formData.get("author");
+    const title = formData.get("bookTitle");
+    let bookId = title;
+    const pages = formData.get("pages");
+    bookId = new Book(...formData.values());
+    addBookToLibrary(bookId);
+    bookForm.reset();
+    // console.log(bookId);
+    // console.log(Object.fromEntries(formData));
+    // console.log(...formData.values());
+});
+
+function addBookToLibrary(book) {
+    // do stuff here
+    myLibrary.push(book);
+    createBookCard(book);
+}
+
+addBookBtn.addEventListener("click", () =>
     // bring out the form
     {
-        formContainer.classList.remove("hide");
+        formContainer.classList.remove("hiden");
     }
 );
 addBook.addEventListener("click", () => {
-    formContainer.classList.add("hide");
+    formContainer.classList.add("hiden");
     // theres no validation if the button hides the form. find a work around
 });
 
@@ -33,59 +57,47 @@ function createBookCard(book) {
     const author = document.createElement("p");
     const title = document.createElement("p");
     const pages = document.createElement("p");
+    const readBtn = document.createElement("button");
+    const delBtn = document.createElement("button");
 
     author.textContent = "Author:";
     title.textContent = "Title:";
     pages.textContent = "Pages:";
+    readBtn.textContent = "Read";
+    delBtn.textContent = "Delete";
 
     bookCard.classList.add("bookCard");
     author.classList.add("author");
     title.classList.add("title");
     pages.classList.add("pages");
+    readBtn.classList.add("readBtn");
+    delBtn.classList.add("delBtn");
 
     bookCard.appendChild(author);
     bookCard.appendChild(title);
     bookCard.appendChild(pages);
+    bookCard.appendChild(readBtn);
+    bookCard.appendChild(delBtn);
     shelf.appendChild(bookCard);
 
     author.textContent += ` ${book.author}`;
     title.textContent += ` ${book.title}`;
     pages.textContent += ` ${book.pages}`;
+
+    bookCard.dataset.author = `${book.author}`;
+    bookCard.dataset.index = myLibrary.length;
+
+    delBtn.addEventListener("click", () => {
+        // alert("sd");
+        
+    });
+
+    console.log(bookCard.dataset.index);
 }
 
-function displayBook(book) {
-    createBookCard(book);
-    // author = document.querySelector(".author");
-    // title = document.querySelector(".title");
-    // pages = document.querySelector(".pages");
-    // author.textContent += ` ${book.author}`;
-    // title.textContent += ` ${book.title}`;
-    // pages.textContent += ` ${book.pages}`;
-}
-
-myLibrary.forEach(displayBook);
-
-bookForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    // create a new formdata object
-
-    const formData = new FormData(bookForm);
-    const author = formData.get("author");
-    const title = formData.get("bookTitle");
-    const pages = formData.get("pages");
-    console.log(author);
-
-    bookForm.reset();
-});
-// add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array. Your code should look something like this:
-
-function addBookToLibrary() {
-    // do stuff here
-}
-
-// Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
-// Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
 // Add a button on each book’s display to remove the book from the library.
 // You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
+
 // Add a button on each book’s display to change its read status.
+
 // To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
